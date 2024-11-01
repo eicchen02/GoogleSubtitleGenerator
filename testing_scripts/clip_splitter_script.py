0e0237acf8fb7a7ebf3049fcf46b497f6b4def27
Eric Chen <eicchen02@gmail.com>
2024-10-31 20:00:51 -0500

    Updated clipper to work with more than .wav

--- testing_scripts/clip_splitter_script.py
+++ testing_scripts/clip_splitter_script.py
@@ -2,20 +2,20 @@ from pydub import AudioSegment
 import os
 
 #? Set file to be shortened
-f = "../temp_aud/conversation.wav"
-basename = os.path.basename(f)
+f = "YOUR_SHOW_FLDR/conversation.mp4"
+basename = os.path.basename(f).split(".")[0]
 
-#?Set time seconds to clip between here
-t1_sec = 15
-t2_sec = 30
+# #?Set time seconds to clip between here
+t1_sec = 30
+t2_sec = 60
 
 t1 = t1_sec * 1000 #Works in milliseconds
 t2 = t2_sec * 1000
 
 try:
-    target_path = '../YOUR_SHOW_FLDR/clipped_{}'.format(basename)
+    target_path = 'YOUR_SHOW_FLDR/clipped_{}.wav'.format(basename)
 
-    newAudio = AudioSegment.from_wav(f)
+    newAudio = AudioSegment.from_file(f)
     newAudio = newAudio[t1:t2]
     newAudio.export(target_path, format="wav") #Exports to a wav file in the current path.
     print("New file created at {}".format(target_path))
