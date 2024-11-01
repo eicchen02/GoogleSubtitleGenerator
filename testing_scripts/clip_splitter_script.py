42982e0ad0337ecd6f59bfb650f649bb3dd627af
Eric Chen <eicchen02@gmail.com>
2024-10-31 20:17:53 -0500

    Added time stamp of clipped audio to name of output

--- testing_scripts/clip_splitter_script.py
+++ testing_scripts/clip_splitter_script.py
@@ -2,18 +2,18 @@ from pydub import AudioSegment
 import os
 
 #? Set file to be shortened
-f = "YOUR_SHOW_FLDR/conversation.mp4"
+f = "YOUR_SHOW_FLDR/conversation2.mp4"
 basename = os.path.basename(f).split(".")[0]
 
 # #?Set time seconds to clip between here
-t1_sec = 30
-t2_sec = 60
+t1_sec = 0
+t2_sec = 127
 
 t1 = t1_sec * 1000 #Works in milliseconds
 t2 = t2_sec * 1000
 
 try:
-    target_path = 'YOUR_SHOW_FLDR/clipped_{}.wav'.format(basename)
+    target_path = 'YOUR_SHOW_FLDR/clipped_{}-{}_{}.wav'.format(t1_sec, t2_sec, basename)
 
     newAudio = AudioSegment.from_file(f)
     newAudio = newAudio[t1:t2]
